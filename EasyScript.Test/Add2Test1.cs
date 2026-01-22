0a6fc8cf6cf2eaaedb9361bd142929813c689167
JavaCommons Technologies <javacommons@gmail.com>
2026-01-22 17:43:00 +0900

    EasyScript-v2026.0122.1742.50

--- EasyScript.Test/Add2Test1.cs
+++ EasyScript.Test/Add2Test1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Global;
-using static Global.EasyObjectClassic;
+using static Global.EasyObject;
 
 
 // ReSharper disable once CheckNamespace
