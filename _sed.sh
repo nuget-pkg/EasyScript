bd917acca14a71a428a5106fd31359bea383ac7d
JavaCommons Technologies <javacommons@gmail.com>
2026-02-09 22:31:29 +0900

    v2026.0209.2231.23

--- _sed.sh
+++ _sed.sh
@@ -6,7 +6,12 @@ cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 version="${ts}"
 
-cd $cwd/EasyScript
-sed -i -e "s/<Version>.*<\/Version>/<Version>${version}<\/Version>/g" EasyScript.csproj
+current=$(cd $(dirname $0);pwd)
+echo $current
+name=`echo "$current" | sed -e 's/.*\/\([^\/]*\)$/\1/'`
+echo $name
+
+cd $cwd/$name
+sed -i -e "s/<Version>.*<\/Version>/<Version>${version}<\/Version>/g" $name.csproj
 cd $cwd/
 echo ${version}>version.txt
